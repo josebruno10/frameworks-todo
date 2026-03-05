50d8490c2b7d9ba86a7d4532a691033d99770297
José Bruno <nevesjosebruno@gmail.com>
2026-03-05 11:00:51 -0400

    update-novinho

---
+++ src/todos/dto/create-todo.dto.ts
@@ -0,0 +1,16 @@
+export class CreateTodoDto {
+  title: string;
+  description: string;
+  completed: boolean;
+  priority: TodoPriority;
+  dueAt: Date;
+  completedAt: Date;
+  userId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+enum TodoPriority {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH',
+}
